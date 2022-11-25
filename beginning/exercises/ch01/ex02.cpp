70dd3497e54a65f834fc0ec6b2309c03b8250140
Relph1119 <huruifeng1202@163.com>
2022-11-25 19:14:38 +0800

    学习C++20入门实践第1章，并完成练习题

---
+++ beginning/exercises/ch01/ex02.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Name: Hu Relph" << std::endl;       // Name
+  std::cout << "Age: 34" << std::endl;             // and age
+}
---
+++ beginning/exercises/ch01/ex03.cpp
@@ -0,0 +1,7 @@
+#include <iostream>                             // 没有使用#
+
+int main()                                      // 应该使用int
+{
+    std::cout << "Hola Mundo!" << std::endl;    // 没有使用分号结尾
+                                                // cout和endl前面都应该是std::
+}                                               // 应使用}
